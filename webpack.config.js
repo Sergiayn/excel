970942189b8849d4971ffb246e10e286fb07d347
Sergiayn <SergeySm128@gmail.com>
2024-10-26 22:06:52 +0300

    Merge pull request #2
    * add HtmlWeboackPlugin, CopyPlugin and extensions, alias
    
    * add loaders and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,60 @@
 const path = require('path')
+const HtmlWeboackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        main: './index.js'
+        main: ['@babel/polyfill','./index.js']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src', 'core'),
+        }
+    },
+    plugins: [
+        new HtmlWeboackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src','favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].bundle.css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 }
