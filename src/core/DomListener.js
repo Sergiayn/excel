8e392aed27c09162ed3f5d9adc605b13d6f9ce8a
Serhii <Sergey202205@gmail.com>
2024-10-28 20:54:29 +0200

    fill components and create dom

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,8 @@
-export class DomListener {}
+export class DomListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('DomListener requires a root element');
+    }
+    this.$root = $root
+  }
+}
