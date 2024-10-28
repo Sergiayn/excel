7b7ff4e2503c27c95dd382a323c74351246a03a5
Serhii <Sergey202205@gmail.com>
2024-10-28 21:27:28 +0200

    add methods to dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,22 +2,22 @@ import {$} from '@core/dom'
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || {}
   }
   getRoot() {
-    const $root =$.create('div', 'excel')
+    const $root = $.create('div', 'excel')
 
     this.components.forEach((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
 
     return $root
   }
-  render(options) {
+  render() {
     this.$el.append(this.getRoot())
   }
 }
