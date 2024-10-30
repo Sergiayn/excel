9e5926a8356026916d380504d46e69e0bedd6add
Serhii <Sergey202205@gmail.com>
2024-10-30 11:50:31 +0200

    show table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,21 +2,22 @@ const CODES = {
   A: 65,
   Z: 90
 }
-// function createCell() {
-//   return `
-//   <div class="cell" contenteditable="">A1</div>
-//   `
-// }
+
+function toCell() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
 
 function toColumn(col) {
   return `
   <div class="column">${col}</div>
   `
 }
-function createRow(content) {
+function createRow(index, content) {
   return `
 <div class="row">
-  <div class="row-info"></div>
+  <div class="row-info">${index ? index : ''}</div>
   <div class="row-data">${content}</div>
 </div>
   `
@@ -35,10 +36,15 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
+    rows.push(createRow(i + 1, cells))
   }
-  console.log(colsCount)
-  return rows.join()
+
+  return rows.join('')
 }
