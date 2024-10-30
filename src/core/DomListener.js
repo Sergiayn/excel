8704321c419176fb7d2c06ec3685b8ebdc51514f
Serhii <Sergey202205@gmail.com>
2024-10-30 10:35:42 +0200

    add removeDOMListeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -17,7 +17,14 @@ export class DomListener {
             `Method ${method} is not implemented in ${this.name} component`
         )
       }
+      this[method] = this[method].bind(this)
       this.$root.on(listener, this[method])
+    })
+  }
+  removeDOMListeners() {
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
     })
   }
 }
