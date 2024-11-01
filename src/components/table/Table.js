152e2005f34b01c35bfbc3de83ad1b327ec56d21
Serhii <Sergey202205@gmail.com>
2024-11-01 22:47:22 +0200

    add resize for row

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,12 +19,19 @@ export class Table extends ExcelComponent {
       const $parent = $resizer.closest('[data-type="resizable"]')
       const coords = $parent.getCoords()
       const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+      const type = $resizer.data.resize
 
       document.onmousemove = (e) => {
-        const delta = e.pageX - coords.right
-        const value = coords.width + delta
-        $parent.$el.style.width = `${value}px`
-        cells.forEach((el) => el.style.width = `${value}px` )
+        if ('col' === type) {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+          $parent.$el.style.width = `${value}px`
+          cells.forEach((el) => el.style.width = `${value}px` )
+        } else {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+          $parent.$el.style.height = `${value}px`
+        }
       }
 
       document.onmouseup = () => {
