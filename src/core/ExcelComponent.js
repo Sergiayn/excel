83b2f4f9b2f90f9d965ce3d05387d5d2f67ca1ed
Serhii <Sergey202205@gmail.com>
2024-11-10 19:31:11 +0200

    optimize storage

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,9 +5,9 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
     this.store = options.store
     this.unsubscribers = []
-    this.storeSub = null
     this.prepare()
   }
   prepare() {}
@@ -28,8 +28,12 @@ export class ExcelComponent extends DomListener {
     this.store.dispatch(action)
   }
 
-  $subscribe(fn) {
-    this.storeSub = this.store.subscribe(fn)
+  storeChanged(changes) {
+    console.log('changes', changes)
+  }
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   init() {
@@ -39,6 +43,5 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach((unsub) => unsub())
-    this.storeSub.unsubscribe()
   }
 }
